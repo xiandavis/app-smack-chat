732706f2a603681ebaad577d36904f2e3c7bda7f
xiandavis <xiandavis@yahoo.com>
2019-02-07 21:54:20 -0800

    added assets

---
+++ Smack/Smack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Christian Davis on 2/7/19.
+//  Copyright © 2019 xianapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
