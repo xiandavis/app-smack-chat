5f43ec1330f856481783196de560d37e4fff2352
xiandavis <xiandavis@yahoo.com>
2019-02-19 16:29:34 -0800

    End of lesson-02

---
+++ Smack/Smack/Controller/ChannelVC.swift
@@ -0,0 +1,22 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Christian Davis on 2/18/19.
+//  Copyright © 2019 xianapps. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60 // STEP 5. experiement with different values here
+
+        // Do any additional setup after loading the view.
+    }
+
+    // Jonny deletes boilerplate code here
+
+}
---
+++ Smack/Smack/Controller/ChatVC.swift
@@ -0,0 +1,31 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Christian Davis on 2/18/19.
+//  Copyright © 2019 xianapps. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    // Outlets - Jonny finds ^dragging from DO to here more accurate than ^dragging from the button in SB to here
+    @IBOutlet weak var menuBtn: UIButton! // STEP 2. Jonny understands it may seem strange to have a button be an outlet rather than an action, but he says we will manually implement the action associated with the button inside viewDidLoad() below
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside) // STEP 3. The param Target is self.revealViewController(), param Selector is a objC method we are going to invoke (the method specifically being revealToggle()--Jonny ⌘clicks revealToggle() to Jump to Definition to read comments above it describing how to use it), param UIControlEvents is .touchUpInside
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer()) // STEP 4. gestureRecognizer: UIGestureRecognizer is self.revealViewController().panGestureRecognizer(), panGestureRecognizer is for slide motion
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer()) // same as above, tapGestureRecognizer is for tap motion
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    // Jonny deletes boilerplate code here
+    
+    
+    
+}
