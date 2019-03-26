af10fc7c18bba530c2624ec27f29532dc2d59b51
xiandavis <xiandavis@yahoo.com>
2019-03-26 11:29:50 -0700

    lesson-10

--- Smack/Smack/Controller/CreateAccountVC.swift
+++ Smack/Smack/Controller/CreateAccountVC.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class CreateAccountVC: UIViewController {
-
+    
+    // Outlets
+    @IBOutlet weak var usernameTxt: UITextField! // STEP 25.
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,6 +23,21 @@ class CreateAccountVC: UIViewController {
     }
 
     // Jonny deletes boilerplate code
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+    }
+    @IBAction func createAccntPressed(_ sender: Any) {
+        guard let email = emailTxt.text , emailTxt.text != "" else { return } // STEP 26. Jonny says the , reads 'if' or 'where' and is another way of unwrapping an optional value
+        guard let pass = passTxt.text , passTxt.text != "" else { return }
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in // hit return at the editor placeholder that now reads 'success' above
+            // a second editor placeholder was here, tabbed to it then deleted and typed below statements
+            if success {
+                print("registered user!")
+            }
+        }
+    }
     @IBAction func closePressed(_ sender: Any) { // STEP 16.
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
---
+++ Smack/Smack/View/RoundedButton.swift
@@ -0,0 +1,34 @@
+//
+//  RoundedButton.swift
+//  Smack
+//
+//  Created by Christian Davis on 3/26/19.
+//  Copyright © 2019 xianapps. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable // STEP 27a.
+class RoundedButton: UIButton {
+
+    // Jonny deletes boilerplate code here
+    @IBInspectable var cornerRadius: CGFloat = 3.0 { // Jonny says needed to affect button appearance in the SB / IB
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() { // Jonny gets autocompletion to bring this statement up by typing 'awakeFromNib'
+        self.setupView() // not defined yet! See below
+    }
+    
+    override func prepareForInterfaceBuilder() { // STEP 28.
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius // STEP 27b.
+    }
+
+}
